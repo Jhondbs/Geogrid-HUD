926ebbfd3f4a104551c87950882bbf78e458baac
Jhondbs <dbsgames.craftplayer@gmail.com>
2026-02-26 16:58:03 -0300

    Update Geo Grid Maps HUD (Atalho)-1.3.user.js

--- Geo Grid Maps HUD (Atalho)-1.3.user.js
+++ Geo Grid Maps HUD (Atalho)-1.3.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GeoGrid Tools (Loader)
 // @namespace    http://tampermonkey.net/
-// @version      8.1
+// @version      8.0
 // @description  Carregador Modular do Sistema Geogrid
 // @author       Jhon
 // @match        http://172.16.6.57/geogrid/aconcagua/*
